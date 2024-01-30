c105067ee7f718ec626a11d718a521ff313d0713
Herik25 <128887958+Herik25@users.noreply.github.com>
2024-01-30 18:08:24 +0530

    updating auth/login to /auth-login

--- routes/Auth.js
+++ routes/Auth.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 const router = express.Router()
 
 router.post('/signup', createUser)
-    .post('/login',passport.authenticate('local'), loginUser)
+    .post('/auth-login',passport.authenticate('local'), loginUser)
     .get('/check',passport.authenticate('jwt'), checkAuth)
 
 exports.router = router;
